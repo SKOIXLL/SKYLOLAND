7a3509250e442f93562698781bd68658bf584f61
SKOIXLL <99454660+SKOIXLL@users.noreply.github.com>
2025-03-10 09:31:28 +0700

    Load.lua

--- Load.lua
+++ Load.lua
@@ -1,6 +1,6 @@
 local GameLoad = nil
 eif game.PlaceId ==  87039211657390 then
-    GameLoad = "Arise Crossover.lua"
+    GameLoad = "Arise Crossover"
 end
 local var,err = pcall(function ()
     loadstring(game:HttpGet("https://github.com/SKOIXLL/SKYLOLAND/main/" .. GameLoad .. ".lua"))()
